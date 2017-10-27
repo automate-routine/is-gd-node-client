88e30378b701bc7260c4b84c75aec717b12c8b0f
Anton Kuznetsov <isnifer@gmail.com>
2017-10-27 10:09:52 +0300

    Add shortener

---
+++ index.js
@@ -0,0 +1,25 @@
+const inquirer = require('inquirer')
+const got = require('got')
+const qs = require('qs')
+
+module.exports = async function start() {
+  const { url } = await inquirer.prompt([{
+    name: 'url',
+    message: 'Get me the url',
+  }])
+
+  const { shorturl } = await inquirer.prompt([{
+    name: 'shorturl',
+    message: 'Tell me short url (optional)',
+  }])
+
+  const params = { format: 'json', url }
+
+  if (shorturl) {
+    params.shorturl = shorturl
+  }
+
+  const response = await got(`https://is.gd/create.php?${qs.stringify(params)}`)
+
+  console.log(response.body.shorturl)
+}
